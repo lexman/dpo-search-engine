834fce51d1c528be50922e5823a9c7249fad7289
Glitch (amenable-swamp-stoat) <none>
2021-04-16 21:50:14 +0000

    Ménage dans la création de la data

--- data/create-dpo-database.sql
+++ data/create-dpo-database.sql
@@ -3,32 +3,32 @@
 DROP TABLE IF EXISTS organisme_dpo;
 
 CREATE TABLE organisme_dpo (
-  organisme_siren	,
-  organisme_nom	,
-  organisme_secteur_activite	,
-  organisme_naf	,
-  organisme_adresse	,
-  organisme_cp	,
-  organisme_ville	,
-  organisme_pays	,
-  type_dpo	,
-  date_designation	,
-  dpo_siren	,
-  dpo_nom	,
-  dpo_secteur_activite	,
-  dpo_naf	,
-  dpo_adresse	,
-  dpo_cp	,
-  dpo_ville	,
-  dpo_pays	,
-  contact_dpo_mail	,
-  contact_dpo_url	,
-  contact_dpo_tel	,
-  contact_dpo_adresse	,
-  contact_dpo_cp	,
-  contact_dpo_ville	,
-  contact_dpo_pays	,
-  contact_dpo_autre	,
+  organisme_siren,
+  organisme_nom,
+  organisme_secteur_activite,
+  organisme_naf,
+  organisme_adresse,
+  organisme_cp,
+  organisme_ville,
+  organisme_pays,
+  type_dpo,
+  date_designation,
+  dpo_siren,
+  dpo_nom,
+  dpo_secteur_activite,
+  dpo_naf,
+  dpo_adresse,
+  dpo_cp,
+  dpo_ville,
+  dpo_pays,
+  contact_dpo_mail,
+  contact_dpo_url,
+  contact_dpo_tel,
+  contact_dpo_adresse,
+  contact_dpo_cp,
+  contact_dpo_ville,
+  contact_dpo_pays,
+  contact_dpo_autre,
   vide
 )
 ;
@@ -38,13 +38,13 @@ CREATE TABLE organisme_dpo (
 .import opencnil-organismes-avec-dpo.csv organisme_dpo
 -- remove header
 DELETE FROM organisme_dpo WHERE rowid=1;
--- remove bad input lines if necessary
-DELETE FROM organisme_dpo WHERE organisme_siren IS NULL;
+-- remove bad input lines if necessary (less than 3 columns on the line)
+DELETE FROM organisme_dpo WHERE organisme_secteur_activit IS NULL;
+
+-- check final content
 .mode csv
 SELECT * FROM organisme_dpo LIMIT 2;
 SELECT COUNT(*) FROM organisme_dpo;
-
 SELECT * FROM organisme_dpo WHERE organisme_nom LIKE "%n%" LIMIT 10;
-
 SELECT COUNT(*)  FROM organisme_dpo WHERE organisme_nom LIKE "%n%" ;
 
--- data/prepare-data.sh
+++ data/prepare-data.sh
@@ -1,3 +1,11 @@
-curl -o opencnil-organismes-avec-dpo.csv -L  https://www.data.gouv.fr/fr/datasets/r/c5d02b42-1008-4406-83f5-3a81c8b936a3
-sqlite3 -init create-dpo-database.sql -echo -batch -bail ../public/.data/data.db ".exit"
-sqlite3 -init create-contrib-database.sql -echo -batch -bail ../public/.data/contribute.db ".exit"
+if [ ! -e ../www/private/data/data.db ]; then
+  curl -o opencnil-organismes-avec-dpo.csv -L  https://www.data.gouv.fr/fr/datasets/r/c5d02b42-1008-4406-83f5-3a81c8b936a3
+	sqlite3 -init create-dpo-database.sql -echo -batch -bail ../www/private/data/data.db ".exit"
+else
+	echo "../www/private/data/data.db already exists... Skipping creation"
+fi
+if [ ! -e ../www/private/data/contribute.db ]; then
+	sqlite3 -init create-contrib-database.sql -echo -batch -bail ../www/private/data/contribute.db ".exit"
+else
+	echo "../www/private/data/contribute.db already exists... Skipping creation"
+fi
